b012995c4cdf57fc78c4513a5f2cf58dd094a319
LBGletsoisa <105360000+LBGletsoisa@users.noreply.github.com>
2022-05-31 03:26:36 +0200

    Add files via upload

---
+++ S3TL.py
@@ -0,0 +1,10 @@
+import urllib
+import re
+from bs4 import BeautifulSoup
+import streamlit as st
+
+st.set_page_config(page_title="My Website", page_icon=":tada", layout="wide")
+st.subheader("Sensor 3")
+
+st.write("Sensor 3 in the basememt crosscut has detetected temperatures lower than the threshold of 35℃")
+
