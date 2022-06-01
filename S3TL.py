2b26d9732d78d2143bf2a2cfcaf229887fab1362
LBGletsoisa <105360000+LBGletsoisa@users.noreply.github.com>
2022-06-01 23:50:42 +0200

    Update S3TL.py

--- S3TL.py
+++ S3TL.py
@@ -6,5 +6,5 @@ import streamlit as st
 st.set_page_config(page_title="My Website", page_icon=":tada", layout="wide")
 st.subheader("Sensor 3")
 
-st.write("Sensor 3 in the basememt crosscut has detetected temperatures lower than the threshold of 35℃")
+st.write("Sensor 3 in the basement crosscut has detected temperatures lower than the threshold of 15℃")
 
